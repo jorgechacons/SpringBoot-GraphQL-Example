d109bf6b23b49cc1d999b319bffe0cb5c2f4e592
jorgechacon04 <jorern@hotmail.com>
2019-09-15 12:23:07 -0600

    Adding flyway

---
+++ src/main/resources/db/migration/V1_02___INSERT_CUSTOMER_DATA.sql
@@ -0,0 +1,7 @@
+INSERT INTO CUSTOMER (user_id,name,last_name) VALUES(1,'Jorge','Chacon');
+INSERT INTO CUSTOMER (user_id,name,last_name) VALUES(2,'Eddie','Vedder');
+INSERT INTO CUSTOMER (user_id,name,last_name) VALUES(3,'Chris','Cornell');
+INSERT INTO CUSTOMER (user_id,name,last_name) VALUES(4,'Dave','Black');
+INSERT INTO CUSTOMER (user_id,name,last_name) VALUES(5,'Mia','Chan');
+INSERT INTO CUSTOMER (user_id,name,last_name) VALUES(6,'Leidy','Chacon');
+INSERT INTO CUSTOMER (user_id,name,last_name) VALUES(7,'Ernesto','Soto');
