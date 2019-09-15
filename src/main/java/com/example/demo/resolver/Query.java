da017b3ce0c22875fa59be589723de5eefc325a7
jorgechacon04 <jorern@hotmail.com>
2019-09-15 13:58:07 -0600

    adding lambda function example

--- src/main/java/com/example/demo/resolver/Query.java
+++ src/main/java/com/example/demo/resolver/Query.java
@@ -5,6 +5,7 @@ import com.example.demo.entity.Customer;
 import com.example.demo.repository.CustomerRepository;
 
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
@@ -24,7 +25,7 @@ public class Query implements GraphQLQueryResolver {
     /**
      * @return All customers
      */
-    public Iterable<Customer> findAllCustomers(){
+    public Iterable<Customer> findAllCustomers() {
         return customerRepository.findAll();
     }
 
@@ -33,7 +34,7 @@ public class Query implements GraphQLQueryResolver {
      * @param id
      * @return Customer ( search by id )
      */
-    public Optional<Customer> findCustomer( Long id) {
+    public Optional<Customer> findCustomer(Long id) {
         return customerRepository.findById(id);
     }
 
@@ -42,9 +43,21 @@ public class Query implements GraphQLQueryResolver {
      * @param id
      * @return all customers that match with the ids
      */
-    public Iterable<Customer> findCustomerByIds(String id){
+    public Iterable<Customer> findCustomerByIds(String id) {
         return customerRepository.findCustomerByIdIn(Arrays.asList(id.split(",")).stream()
                 .map(Long::parseLong).collect(Collectors.toList()));
     }
 
+    /**
+     * @param startsWith
+     * @return filter all the customers that the name matches with the condition
+     */
+    public Iterable<Customer> findCustomersNameStartsWith(String startsWith) {
+        return customerRepository.findAll().stream()
+                .filter((x) -> x.getName().startsWith(startsWith.toUpperCase()))
+                .sorted(Comparator.comparing(Customer::getName)).collect(Collectors.toList());
+    }
+
 }
+
+
--- src/main/resources/db/migration/V1_02___INSERT_CUSTOMER_DATA.sql
+++ src/main/resources/db/migration/V1_02___INSERT_CUSTOMER_DATA.sql
@@ -3,5 +3,6 @@ INSERT INTO CUSTOMER (user_id,name,last_name) VALUES(2,'Eddie','Vedder');
 INSERT INTO CUSTOMER (user_id,name,last_name) VALUES(3,'Chris','Cornell');
 INSERT INTO CUSTOMER (user_id,name,last_name) VALUES(4,'Dave','Black');
 INSERT INTO CUSTOMER (user_id,name,last_name) VALUES(5,'Mia','Chan');
-INSERT INTO CUSTOMER (user_id,name,last_name) VALUES(6,'Jose','Chacon');
+INSERT INTO CUSTOMER (user_id,name,last_name) VALUES(6,'Jose','Brenes');
 INSERT INTO CUSTOMER (user_id,name,last_name) VALUES(7,'Ernesto','Soto');
+INSERT INTO CUSTOMER (user_id,name,last_name) VALUES(8,'Javier','Delgado');
